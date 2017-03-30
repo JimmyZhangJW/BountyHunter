356b7b8f6cd76767559b05c7f9adadd7f4aae4c5
MalcolmChen97 <502829548@qq.com>
2017-03-30 10:47:49 -0700

    basic

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,24 @@
 package Main;
 
-public class Main {
+import MissionHolder.*;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JFrame;
 
+public class Main {
+    
     public static void main(String[] args) {
-	/// / write your code here
-  //TODO
+       
+        DatabaseConnect myconnect=new DatabaseConnect("ora_b9j0b","a25724155");
+        myconnect.connect();
+        
+        JFrame mainframe = new signup();
+        mainframe.setVisible(true);
+
     }
 }
