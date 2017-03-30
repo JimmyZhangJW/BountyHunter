5a9c1153edece8caa01bdee0a42cceb18a1f2c63
odzick <odzick@gmail.com>
2017-03-30 00:39:58 -0700

    commit before pull request

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,10 +6,6 @@ import static Main.Connector.getConnection;
 import javax.swing.JFrame;
 
 public class Main {
-
-   // --- FILL THESE IN ---
-    static String ORA_ACCOUNT = "ora_j5k0b";
-    static String ORA_PASSWORD = "a36579143";
     
     public static void main(String[] args) {
         JFrame frame = new JFrame();
