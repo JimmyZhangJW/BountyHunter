0c121d95a218ecc3849962e5e8c5c270f3260723
JimmyZhangJW <z1771485029@gmail.com>
2017-03-30 19:05:26 -0700

    Revert "."
    This reverts commit c7289848d00b25d1ac578137be1ac807020e7608, reversing
    changes made to a1ce5b6be452c534e740cc8479b78fabb2314e45.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,6 +1,5 @@
 package Main;
 
-<<<<<<< HEAD
 import java.sql.*;
 import javax.swing.JFrame;
 import Hunter.*;
@@ -44,26 +43,5 @@ public class Main {
         hunterGui.setVisible(true);
         
         
-=======
-import MissionHolder.*;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import Leaderboard.LeaderBoardPanel;
-import static Main.Connector.closeConnection;
-import static Main.Connector.getConnection;
-
-import javax.swing.JFrame;
-
-public class Main {
-    
-    public static void main(String[] args) {        
-        JFrame mainframe = new signup();
-        mainframe.setVisible(true);
->>>>>>> master
     }
 }
